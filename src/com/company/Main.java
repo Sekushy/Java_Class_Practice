a2e647d93a7ee94cd3e2752fcfca704d13b750b6
Mihai F <cochior.mihaela@yahoo.com>
2021-12-04 12:39:04 +0200

    Create person class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//
+        Person one = new Person("Draghici","Andrei",20);
+        System.out.println(one.getAge());
+        System.out.println(one.getFirstName());
     }
 }
