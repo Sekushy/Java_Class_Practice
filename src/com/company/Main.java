c2e70a9344edae316828ad8df26f72824035ce8f
Radu <radu@example.com>
2021-12-04 12:39:13 +0200

    Create person class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person one = new Person("Radu","Andrei",24);
+        System.out.println(one.getAge());
+        System.out.println(one.getFirstName());
+        System.out.println(one.getLastName());
     }
 }
