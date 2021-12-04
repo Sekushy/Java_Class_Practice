b2b37180d995a88b51135ae4f4ccc77fd02371a4
Gabriel Luican <gabriel.luican@gmail.com>
2021-12-04 12:52:58 +0200

    My person is Marcel Pavel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person person = new Person("Marcel", "Pavel", 24);
+        System.out.println(person.getAge());
+        System.out.println(person.getLastName());
+        System.out.println(person.getAge());
     }
 }
